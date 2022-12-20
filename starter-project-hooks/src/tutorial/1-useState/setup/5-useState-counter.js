3449d5887a0fee115e30e333cd76e48819fe9f7a
Sneha1203 <78474043+Sneha1203@users.noreply.github.com>
2022-12-20 23:52:51 +0530

    birthday reminder project

--- starter-project-hooks/src/tutorial/1-useState/setup/5-useState-counter.js
+++ starter-project-hooks/src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,36 @@
 import React, { useState } from 'react';
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [value, setValue] = useState(0)
+
+  const increaseValue = () => {
+    setTimeout(() => {
+      setValue((prevValue) => {
+        return prevValue + 1
+      })
+    }, 2000)
+    
+  }
+
+  const resetValue = () => {
+    setValue(0)
+  }
+
+  const decreaseValue = () => {
+    setValue((prevValue) => {
+      return prevValue - 1
+    })
+  }
+
+  return (
+    <>
+      <h2>Regular counter</h2>
+      <h2>{value}</h2>
+      <button type='button' className='btn' onClick={increaseValue}>Increase</button>
+      <button type='button' className='btn' onClick={resetValue}>Reset</button>
+      <button type='button' className='btn' onClick={decreaseValue}>Decrease</button>
+    </>
+  )
 };
 
 export default UseStateCounter;
